ab8ab1874b2f4b6c9ad73d54b335e1b2747c1f09
szezso <szezsoo@gmail.com>
2014-05-10 15:00:22 +0200

    Expressltexx: Initial upload from arco

---
+++ cm.mk
@@ -0,0 +1,15 @@
+$(call inherit-product, device/samsung/expressltexx/full_expressltexx.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=expressltexx TARGET_DEVICE=expresslte BUILD_FINGERPRINT="samsung/expressltexx/expresslte:4.1.2/JZO54K/I8730XWAMK1:user/release-keys" PRIVATE_BUILD_DESC="expressltexx-user 4.1.2 JZO54K I8730XWAMK1 release-keys"
+
+PRODUCT_DEVICE := expressltexx
+PRODUCT_NAME := cm_expressltexx
